33d4462dc129b207fc7267d39af6faf619f7b4ea
Debbie Ellis <debbieellis7@gmail.com>
2019-06-23 15:48:41 +0800

    Get & Delete Post Routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { check, validationResult } = require('express-validator/check')
 const auth = require('../../middleware/auth')
 
+// Models
 const Post = require('../../models/Post')
 const Profile = require('../../models/Profile')
 const User = require('../../models/User')
@@ -45,5 +46,67 @@ router.post(
     }
   }
 )
+
+// @route   GET api/posts
+// @desc    GET all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 })
+    res.json(posts)
+  } catch(err) {
+    console.error(err.message)
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   GET api/posts/:id
+// @desc    GET posts by ID
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    res.json(post)
+  } catch(err) {
+    console.error(err.message)
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+})
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+
+    // Check user
+    if(post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' })
+    }
+
+    await post.remove()
+
+    res.json({ msg: 'Post removeed' })
+  } catch(err) {
+    console.error(err.message)
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' })
+    }
+    res.status(500).send('Server Error')
+  }
+})
 
 module.exports = router
