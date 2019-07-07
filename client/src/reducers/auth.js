8f4db45aff917aa6072f8214e75dea8355de0b14
Debbie Ellis <debbieellis7@gmail.com>
2019-07-07 17:51:06 +0800

    User Login

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -2,7 +2,9 @@ import {
   REGISTER_SUCCESS,
   REGISTER_FAIL,
   USER_LOADED,
-  AUTH_ERROR
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL
 } from '../actions/types'
 
 const initialState = {
@@ -25,8 +27,8 @@ export default function(state = initialState, action) {
       }
 
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token)
-
       return {
         ...state,
         ...payload,
@@ -36,6 +38,7 @@ export default function(state = initialState, action) {
 
     case REGISTER_FAIL:
     case AUTH_ERROR:
+    case LOGIN_FAIL:
       localStorage.removeItem('token')
       return {
         ...state,
