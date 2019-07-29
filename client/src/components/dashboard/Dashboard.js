9ea1fdd3fb924d066e953d2bf8e6b7764d9991e1
debbieellis7 <debbieellis7@gmail.com>
2019-07-29 19:14:13 +0800

    Edit Profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
+import DashboardActions from './DashboardActions'
 import { getCurrentProfile } from '../../actions/profile'
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
@@ -10,20 +11,26 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
   getCurrentProfile()
  }, [])
 
- return loading && profile === null ? <Spinner /> : <Fragment>
-  <h1 className="large text-primary">Dashboard</h1>
-  <p className="lead">
-   <i className="fas fa-user"></i> Welcome { user && user.name}
-  </p>
-  { profile !== null ? (
-   <Fragment>has</Fragment>
-  ) : (
+ return loading && profile === null ? (
+   <Spinner />
+ ) : (
    <Fragment>
-    <p>You have not yet setup a profile, please add some info</p>
-    <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link> 
+    <h1 className="large text-primary">Dashboard</h1>
+    <p className="lead">
+     <i className="fas fa-user"></i> Welcome { user && user.name}
+    </p>
+    { profile !== null ? (
+      <Fragment>
+       <DashboardActions />
+      </Fragment>
+    ) : (
+      <Fragment>
+       <p>You have not yet setup a profile, please add some info</p>
+       <Link to="/create-profile" className="btn btn-primary my-1">Create Profile</Link>
+      </Fragment>
+    )}
    </Fragment>
-  )}
- </Fragment>
+ )
 }
 
 Dashboard.propTypes = {
